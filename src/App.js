ac7b5b0a38b3c8f7ea981b9a1854b8c6d8a5a66b
Tenzin Lekphell <61218333+tenzinlekphell@users.noreply.github.com>
2022-08-10 17:31:05 -0400

    Import app.css into App.js and added react dev tools script tag to index.html page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import EducationalInfo from './components/EducationalInfo';
 import GeneralInfo from './components/GeneralInfo';
 import PracticalExp from './components/PracticalExp';
+import './styles/app.css';
 
 function App() {
   return (
