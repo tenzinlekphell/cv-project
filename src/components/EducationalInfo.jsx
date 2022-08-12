e6248f6997a037aa6d9e3fa9fa1eb68f1810fe58
Tenzin Lekphell <61218333+tenzinlekphell@users.noreply.github.com>
2022-08-11 22:45:14 -0400

    Create input states and handler functions for EducationalInfo component

--- src/components/EducationalInfo.jsx
+++ src/components/EducationalInfo.jsx
@@ -1,19 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const EducationalInfo = () => {
+  const [school, setSchool] = useState('');
+  const [studyTitle, setStudyTitle] = useState('');
+  const [educationInfo, setEducationInfo] = useState([]);
+
+  const schoolNameHandler = (e) => {
+    setSchool(e.target.value);
+  };
+
+  const studyNameHandler = (e) => {
+    setStudyTitle(e.target.value);
+  };
+
+  const addInfo = () => {
+    setEducationInfo([...educationInfo, school, studyTitle]);
+  };
+
+  const handleSubmit = (e) => {
+    alert('Submission successful!');
+    e.preventDefault();
+    setStudyTitle('');
+    setSchool('');
+  };
   return (
     <div>
       <h1>Educational Experience</h1>
-      <form action="" className="form-container">
+      <form action="" onSubmit={handleSubmit} className="form-container">
         <label htmlFor="">School Name</label>
-        <input type="text" />
+        <input type="text" value={school} onChange={schoolNameHandler} />
         <label htmlFor="">Title of study</label>
-        <input type="text" />
+        <input type="text" value={studyTitle} onChange={studyNameHandler} />
         <label htmlFor="">Date of study</label>
         <input type="date" />
         <button>Edit</button>
-        <button>Submit</button>
+        <button onClick={addInfo}>Submit</button>
       </form>
+
+      <div>
+        <p>School Name: {educationInfo[0]}</p>
+        <p>Title of Study: {educationInfo[1]}</p>
+      </div>
     </div>
   );
 };
